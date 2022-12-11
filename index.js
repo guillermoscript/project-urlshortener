5e7a135e4b859f6dcbfd7bff22c88aebfae2b267
Guillermo <guillermo.marin@roraimadevs.com>
2022-12-11 17:48:30 -0400

    added body parser and api route methods

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { shorten, goTo } = require('./services/url')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+// create this array to store the urls in memory
+const urls = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +15,27 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body;
+  const data = shorten(url,urls);
+  res.json(data);
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params;
+  const url = goTo(id,urls);
+  res.redirect(url)
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
